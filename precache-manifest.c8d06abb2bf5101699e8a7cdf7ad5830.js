99b7e821e940da9efad32837cba997d121e26ec1
joyjitdas10 <joyjitdas26@gmail.com>
2020-10-16 10:46:57 +0530

    Updates

---
+++ precache-manifest.c8d06abb2bf5101699e8a7cdf7ad5830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf4c9138e311d3a758037994df7d6bd",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "5e698ecab8522d46499b",
+    "url": "/Monster-rolodex/static/css/main.dbc5d3a0.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/Monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e698ecab8522d46499b",
+    "url": "/Monster-rolodex/static/js/main.1e5f55d3.chunk.js"
+  },
+  {
+    "revision": "405800b5fe7a1c0159aa",
+    "url": "/Monster-rolodex/static/js/runtime-main.3102fe84.js"
+  }
+]);
